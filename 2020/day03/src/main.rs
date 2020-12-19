f6530190c1bfb93a17cfb308ae97ec693a7c5c7c
Casey Primel <casey.primel@gmail.com>
2020-12-19 10:08:23 -0500

    Day 3, Part 2

--- 2020/day03/src/main.rs
+++ 2020/day03/src/main.rs
@@ -1,29 +1,53 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{copy,BufRead, BufReader};
+
+fn count_trees_on_path(horizontal_move: usize, vertical_shift:usize, slope: &Vec<u8>) -> i32 {
+    let mut tree_count = 0;
+    let mut horizontal_pos = 1;
+    let mut vertical_counter = vertical_shift;
+
+    for row in slope.lines() {
+        if vertical_counter == vertical_shift {
+            let row = row.unwrap();
+            let row_bytes = row.as_bytes();
+            let row_size = row_bytes.len();
+
+            if horizontal_pos > row_size {
+                horizontal_pos = horizontal_pos - row_size;
+            }
+
+            if row_bytes[horizontal_pos - 1] == b'#' {
+                tree_count += 1;
+            }
+            horizontal_pos += horizontal_move;
+            vertical_counter = 1;
+        } else {
+            vertical_counter += 1;
+        }
+
+
+    }
+
+    tree_count
+}
 
 fn main() {
     let filename = "input.txt";
     let file = File::open(filename).unwrap();
-    let reader = BufReader::new(file);
+    let mut reader = BufReader::new(file);
+    let mut writer: Vec<u8> = vec![];
+    copy(&mut reader, &mut writer);
 
-    let mut tree_count = 0;
-    let mut horizontal_pos = 1;
+    let mut result: i32 = 1;
 
-    let horizontal_move = 3;
 
-    for line in reader.lines() {
-        let line = line.unwrap();
-        let bytes = line.as_bytes();
-        let len_segment = bytes.len();
+    println!("{}", count_trees_on_path(3,1,&writer));
 
-        if horizontal_pos > len_segment {
-            horizontal_pos = horizontal_pos - len_segment;
-        }
+    result *= count_trees_on_path(1,1,&writer);
+    result *= count_trees_on_path(3,1,&writer);
+    result *= count_trees_on_path(5,1,&writer);
+    result *= count_trees_on_path(7,1,&writer);
+    result *= count_trees_on_path(1,2,&writer);
 
-        if bytes[horizontal_pos - 1] == b'#' {
-            tree_count += 1;
-        }
-        horizontal_pos += horizontal_move;
-    }
-    println!("{}", tree_count);
+    println!("{}", result);
 }
