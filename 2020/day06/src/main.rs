856a2e96abc25becfb1557448d2721b7f09ea12a
Casey Primel <casey.primel@gmail.com>
2021-01-01 12:14:57 -0500

    Day 06, Part 2

--- 2020/day06/src/main.rs
+++ 2020/day06/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 fn load_input(filename: &str) -> BufReader<File> {
     let file = File::open(filename).unwrap();
@@ -11,21 +11,40 @@ fn main() {
     let input = "input";
     let reader = load_input(input);
 
-    let mut answers: HashSet<u8> = HashSet::new();
-    let mut sum = 0;
+    let mut answers: HashMap<u8, i32> = HashMap::new();
+    let mut sum_1 = 0;
+    let mut sum_2 = 0;
+    let mut counter = 0;
 
     for line in reader.lines() {
         let line = line.unwrap();
 
         if line.is_empty() {
-            sum += answers.len();
+            sum_1 += answers.len();
+            for (_key, val) in answers.iter() {
+                if *val == counter {
+                    sum_2 += 1;
+                }
+            }
+            counter = 0;
             answers.clear();
         } else {
-            for &c in line.as_bytes() {
-                answers.insert(c);
+            counter += 1;
+            for c in line.as_bytes() {
+                let mut new_val = 1;
+                if answers.contains_key(c) {
+                    new_val += answers.get(c).unwrap();
+                }
+                answers.insert(*c, new_val);
             }
         }
     }
-    sum += answers.len();
-    println!("The sum of affirmative responses is {}", sum);
+    sum_1 += answers.len();
+    for (_key, val) in answers.iter() {
+        if *val == counter {
+            sum_2 += 1;
+        }
+    }
+    println!("The sum of all affirmative responses is {}", sum_1);
+    println!("The sum of questions to which individual groups' members had all affirmative responses is {}", sum_2);
 }
